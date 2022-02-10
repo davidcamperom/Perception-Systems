843add44061639a14994ef8dab7edf09410e0dbe
David Campero Maña <davidcampero26@gmail.com>
2022-02-10 16:52:56 +0100

    Delete Color tracking work directory

--- Color tracking work/Algoritmo_Pruebas/08_AlgoritmoVideo_SoloColor/Funciones/carga_video_entrada.m
+++
@@ -1,6 +0,0 @@
-function [numFrames, numFilasFrame, numColumnasFrame, FPS]  = carga_video_entrada(videoInput)
-    FPS = videoInput.FrameRate;
-    numFrames = videoInput.NumFrames;
-    numFilasFrame = videoInput.Height;
-    numColumnasFrame = videoInput.Width;
-end
